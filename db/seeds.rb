6f8238eb01bcc2f3d5c5af51bd87452210762c39
Student <example@example.com>
2020-01-16 15:29:19 -0800

    add friday functionality, save for scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Employee.destroy_all
+
+50.times do |index|
+  Employee.create!(name: Faker::Games::Pokemon.name)
+end
+
+p "created #{Employee.count} Pokemon"
